54a856e4780b37bfb5a48c00e05606b959fdc0cf
himcode2 <94326944+himcode2@users.noreply.github.com>
2024-10-07 09:10:09 +0530

    Update 01_variables.js

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,7 +2,7 @@ const accountId = 144553
 let accountEmail = "hitesh@google.com"
 var accountPassword = "12345"
 accountCity = "Jaipur"
-let accountState;
+let accountState; // this is print undefined
 
 // accountId = 2 // not allowed
 
@@ -18,5 +18,5 @@ Prefer not to use var
 because of issue in block scope and functional scope
 */
 
-
+//Except for accountId every other variable will change there values
 console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
