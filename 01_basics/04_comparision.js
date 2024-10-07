954e958f83db6de6454a1874812627909e7726ad
himcode2 <94326944+himcode2@users.noreply.github.com>
2024-10-07 09:49:16 +0530

    Update 04_comparision.js

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -8,14 +8,18 @@
 // console.log("2" > 1);
 // console.log("02" > 1);
 
-console.log(null > 0);
-console.log(null == 0);
-console.log(null >= 0);
+// The reason why comparison and equality check works diffrenetly is that 
+// Comparison coverts ull to number, treating it as zero
+// Whereas equality check dosent convert null to a number 
+console.log(null > 0); //false
+console.log(null == 0); // false
+console.log(null >= 0); // true
 
-console.log(undefined == 0);
-console.log(undefined > 0);
-console.log(undefined < 0);
+// undefined is not converted to a number in both the checks.
+console.log(undefined == 0); //fasle
+console.log(undefined > 0); //false
+console.log(undefined < 0); //false
 
-// === 
-
-console.log("2" === 2);
+// === this checks the type as well as its value whereas == checks only value 
+console.log("2" === 2); // false
+console.log("2" == 2); // true
